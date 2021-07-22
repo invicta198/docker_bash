638e2ede1234c984d9f2b093ed92a3f2bcad7c29
Avijit Pandey <avijit@voereir.com>
2021-07-22 15:38:36 +0530

    General Bug Fixes.

--- git_pull.sh
+++ git_pull.sh
@@ -10,8 +10,7 @@ function common_pull(){
 	echo 'Pulling Common repo !...'
 	repo=touchstone-common
 	delete_cache $repo touchstone_common
-	cd $repo
-	github_pull
+	github_pull $repo master
 	source ~/voereir/code/ts-venv/bin/activate
 	cd touchstone-common/touchstone_common/db/migration
 	command=`alembic downgrade -1 2>&1 `
@@ -23,8 +22,7 @@ function agent_pull(){
 	echo 'Pulling Agent repo !...'
 	repo=touchstone-agent-webservice
 	delete_cache $repo .
-	cd $repo
-	github_pull
+	github_pull $repo master
 	source touchstone-agent-webservice/server/venv3/bin/activate
 	deactivate
 }
@@ -32,51 +30,44 @@ function package_pull(){
 	echo 'Pulling Package repo !...'
 	repo=touchstone-package
 	delete_cache $repo .
-	cd $repo
-	github_pull
+	github_pull $repo master
 }
 function api_pull(){
 	echo 'Pulling Api repo !...'
 	repo=touchstone-api
 	delete_cache $repo touchstoneApi
-	cd $repo
-	github_pull	
+	github_pull $repo develop	
 }
 function doc_pull(){
 	echo 'Pulling Doc repo !...'
 	repo=touchstone-doc
 	delete_cache $repo .
-	cd $repo
-	github_pull
+	github_pull $repo master
 }
 function web_pull(){
 	echo 'Pulling Web repo !...'
 	repo=touchstone-web
 	delete_cache $repo .
-	cd $repo
-	github_pull
+	github_pull $repo master
 	command=`bash touchstone-web/webpack/pack-webpack.cmd 2>&1 `
 }
 function cli_pull(){
 	echo 'Pulling CLI repo !...'
 	repo=touchstone-CLI
 	delete_cache $repo touchstone_cli
-	cd $repo
-	github_pull
+	github_pull $repo master
 }
 function externals_pull(){
 	echo 'Pulling Externals repo !...'
 	repo=touchstone-externals
 	delete_cache $repo touchstone
-	cd $repo
-	github_pull
+	github_pull $repo master
 }
 function management_pull(){
 	echo 'Pulling Management repo !...'
 	repo=touchstone-management-portal
 	delete_cache $repo touchstone_management_portal
-	cd $repo
-	github_pull
+	github_pull $repo master
 	source ~/voereir/code/ts-venv/bin/activate
 	cd touchstone-management-portal/touchstone_management_portal/common/db/migration
 	command=`alembic downgrade -1 2>&1 `
@@ -88,18 +79,19 @@ function engine_pull(){
 	echo 'Pulling Engine repo !...'
 	repo=touchstone-engine
 	delete_cache $repo ve_engines
-	cd $repo
-	github_pull
+	github_pull $repo master
 }
 function github_pull(){
-	command=`git checkout master 2>&1 `
+	cd $1
+	requested_branch=$2
+	command=`git checkout $requested_branch 2>&1 `
 	if [[ $command == *'Please commit your changes or stash them before you switch branches.'* ]]; then
   		user_git_branch=`git rev-parse --abbrev-ref HEAD 2>&1 ` #Git 1.6.3 or newer.
   		echo 'You are on branch: '$user_git_branch
   		echo 'Press Y: stash and apply || Press N: stash and drop'
   		read user_option
   		command=`git stash 2>&1 `
-  		command=`git checkout master 2>&1 `
+  		command=`git checkout $requested_branch 2>&1 `
   		command=`git pull 2>&1 `
   		command=`git checkout $user_git_branch 2>&1 `
   		if [ "$user_option" = "Y" ] || [ "$user_option" = "y" ]; then
